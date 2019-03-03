0e7711685d6ee8adf0625fb129e2093f05a0e94f
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-03 01:41:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(veggies)# code an argument here
 
 end
 
-def long_planteer_calls(long_planteer_calls)
+def long_planeteer_calls(long_planteer_calls)
   answer = false
   long_planteer_calls.each do |call|
     if call.length > 4
